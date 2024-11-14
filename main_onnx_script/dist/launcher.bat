c802845d7d52f44fa4b1fd772de1075437707c8d
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-11-14 15:30:50 -0500

    Update launcher.bat
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- main_onnx_script/dist/launcher.bat
+++ main_onnx_script/dist/launcher.bat
@@ -4,7 +4,7 @@ pushd %~dp0
 
 REM Run the Python script and check for errors
 echo Running main_onnx.py...
-python "C:\Users\rubbe\OneDrive\Desktop\Assistive-AimGuide-main\Assistive-AimGuide-main\main_onnx_script\dist\main_onnx.py"
+python PASTE THE PATH TO THE MAIN_ONNX.PY
 if %errorlevel% neq 0 (
     echo Error: main_onnx.py did not run successfully. Error level: %errorlevel%
     popd
