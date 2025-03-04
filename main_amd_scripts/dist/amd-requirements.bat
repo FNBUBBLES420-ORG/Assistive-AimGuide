0c3da2c95e1fbc2e5d72458bb857d6b4d37b89f9
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2025-03-04 18:56:02 -0500

    Delete main_amd_scripts/dist directory
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- main_amd_scripts/dist/amd-requirements.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Installing required packages...
-
-pip install -r amd_requirements.txt > NUL
-
-echo Done!
-pause
--- main_amd_scripts/dist/pyarmor_runtime_0000001/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 8.5.11 (trial), 0000001, 2024-09-26T14:47:12.537440
-from .pyarmor_runtime import __pyarmor__
