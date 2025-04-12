841bd826694df0c05011f9abae9e33412ead1142
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2025-04-11 21:26:27 -0400

    Update config.py
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- Main-NVIDIA-Users/main_tensorrt_script/dist/config.py
+++ Main-NVIDIA-Users/main_tensorrt_script/dist/config.py
@@ -44,7 +44,7 @@ onnxChoice = 3
 # Uncomment the appropriate model path depending on which model you're using put # in front of the model_path
 
 # For TensorRT engine model
-model_path = 'v5.engine'
+model_path = 'engine-models/v5.engine'
 
 # For ONNX model
 # model_path = 'v5.onnx'
