c5deca14b966ab2cedfe0de9579364ec406cd150
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2025-04-20 06:16:07 -0400

    Update update_ultralytics.bat
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- update_ultralytics.bat
+++ update_ultralytics.bat
@@ -2,7 +2,7 @@
 echo Regularly check for updated Ultralytics versions.
 echo.
 pause
-echo press enter to continue.
+
 REM Check if Python is installed
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
